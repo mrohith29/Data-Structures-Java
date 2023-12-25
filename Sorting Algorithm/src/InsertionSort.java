2b57427d355b186dda3326faa8dd164912f8d4c2
mrohith29 <mariyalarohith29@gmail.com>
2023-12-25 21:20:54 +0530

    add insertion sort

--- Sorting Algorithm/src/InsertionSort.java
+++ Sorting Algorithm/src/InsertionSort.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class InsertionSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the size of inputs : ");
         int n = sc.nextInt();
         int[] input = new int[n];
         for (int i = 0; i < n; i++) {
@@ -9,6 +10,19 @@ public class InsertionSort {
                 input[i] = sc.nextInt();
             }
         }
+        for (int i = 1; i < n; i++) {
+            int temp = input[i];
+            int j = i;
+            while (j > 0 && input[j - 1] > temp) {
+                input[j] = input[j - 1];
+                j--;
+            }
+            input[j] = temp;
+        }
 
+        System.out.println("Sorted list of elements : ");
+        for (int i : input) {
+            System.out.print(i+" ");
+        }
     }
 }
